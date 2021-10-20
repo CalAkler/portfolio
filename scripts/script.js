44774cb62b76f8bd2ad1e15b2b51f18f9954e514
CalAkler <calvinakler@gmail.com>
2021-10-20 19:41:05 -0400

    nav bar animations and header styles

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,30 @@ AOS.init();
 
 
 
+// HIDDE NAV
+const nav = document.querySelector('.desktopNav');
+let previousScrollPosition = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+  let currentScrollPosition = window.pageYOffset;
+  if (previousScrollPosition < currentScrollPosition) {
+    nav.classList.add('hideNav');
+  } else {
+    nav.classList.remove('hideNav');
+  }
+
+  prevScrollPosition = currentScrollPosition;
+});
+
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
