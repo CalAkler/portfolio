263b9845283fb1514d70444062d98cb72ecefca1
CalAkler <calvinakler@gmail.com>
2021-10-21 15:25:31 -0400

    project screens working on smaller device

--- scripts/projectInfo.js
+++ scripts/projectInfo.js
@@ -5,52 +5,68 @@ const proj3 = document.querySelector('.proj3');
 const proj4 = document.querySelector('.proj4');
 
 // get project info boxes to be displayed
-const info1 = document.querySelector('#proj1Display');
-const info2 = document.querySelector('#proj2Display');
-const info3 = document.querySelector('#proj3Display');
-const info4 = document.querySelector('#proj4Display');
+const info1 = document.querySelectorAll('#proj1Display');
+const info2 = document.querySelectorAll('#proj2Display');
+const info3 = document.querySelectorAll('#proj3Display');
+const info4 = document.querySelectorAll('#proj4Display');
 
-// put on page when user hovers 
+// put on page when user hovers, remove when mouse leaves 
 // wdyn
 proj1.addEventListener('mouseover', () => {
-  info1.classList.add('wdynInfo');
-  info1.classList.remove('hidden');
+  for (let i = 0; i < info1.length; i++) {
+    info1[i].classList.add('wdynInfo');
+    info1[i].classList.remove('hidden');
+  }
 });
 
 proj1.addEventListener('mouseout', () => {
-  info1.classList.remove('wdynInfo');
-  info1.classList.add('hidden');
+  for (let i = 0; i < info1.length; i++) {
+    info1[i].classList.remove('wdynInfo');
+    info1[i].classList.add('hidden');
+  }
 });
 
 // mad-libs
 proj2.addEventListener('mouseover', () => {
-  info2.classList.add('madLibsInfo');
-  info2.classList.remove('hidden');
+  for (let i = 0; i < info2.length; i++) {
+    info2[i].classList.add('madLibsInfo');
+    info2[i].classList.remove('hidden');
+  }
 });
 
 proj2.addEventListener('mouseout', () => {
-  info2.classList.remove('madLibsInfo');
-  info2.classList.add('hidden');
+  for (let i = 0; i < info2.length; i++) {
+    info2[i].classList.remove('madLibsInfo');
+    info2[i].classList.add('hidden');
+  }
 });
 
 // planted
 proj3.addEventListener('mouseover', () => {
-  info3.classList.add('plantedInfo');
-  info3.classList.remove('hidden');
+  for (let i = 0; i < info3.length; i++) {
+    info3[i].classList.add('plantedInfo');
+    info3[i].classList.remove('hidden');
+  }
 });
 
 proj3.addEventListener('mouseout', () => {
-  info3.classList.remove('plantedInfo');
-  info3.classList.add('hidden');
+  for (let i = 0; i < info3.length; i++) {
+    info3[i].classList.remove('plantedInfo');
+    info3[i].classList.add('hidden');
+  }
 });
 
 // rhymeTime
 proj4.addEventListener('mouseover', () => {
-  info4.classList.add('rhymeTimeInfo');
-  info4.classList.remove('hidden');
+  for (let i = 0; i < info4.length; i++) {
+    info4[i].classList.add('rhymeTimeInfo');
+    info4[i].classList.remove('hidden');
+  }
 });
 
 proj4.addEventListener('mouseout', () => {
-  info4.classList.remove('rhymeTimeInfo');
-  info4.classList.add('hidden');
+  for (let i = 0; i < info4.length; i++) {
+    info4[i].classList.remove('rhymeTimeInfo');
+    info4[i].classList.add('hidden');
+  }
 });
